8c9069ca7732110c33722120e4aa490d2fd8b47b
hestia <minibrary@gmail.com>
2016-09-27 23:12:09 +0900

    image url modified

--- _posts/sed.sh
+++ _posts/sed.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-for file in ls *
+for file in ls *.md
 do
     if [ -f $file ]
     then
-        tmpfile="$file.tmp"
-        sed -e 's/"https://googledrive.com/host/0Bw2KEQNBe4nMZW91OWJNZ2lmX0k"/"http://minibrary.com/blogimg"/g' $file > $tmpfile
+        sed -i 's,https://googledrive.com/host/0Bw2KEQNBe4nMZW91OWJNZ2lmX0k,http://minibrary.com/blogimg,g' $file
     fi
 done
