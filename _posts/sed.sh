3b31e16164c56705b7f21e12604fd70481a0487c
Minibrary <minibrary@gmail.com>
2017-10-12 21:09:45 +0900

    Image URL modified for HTTPS

--- _posts/sed.sh
+++ _posts/sed.sh
@@ -4,6 +4,6 @@ for file in ls *.md
 do
     if [ -f $file ]
     then
-        sed -i 's,https://googledrive.com/host/0Bw2KEQNBe4nMZW91OWJNZ2lmX0k,http://minibrary.com/blogimg,g' $file
+        sed -i 's,http://minibrary.com/,https://minibrary.com,g' $file
     fi
 done
